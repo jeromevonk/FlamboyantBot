2fe90bc34083ddd3103a0d74feac81f87e1ae123
Jerome Vonk <jeromevonk@users.noreply.github.com>
2018-07-17 23:48:25 -0300

    messages

--- app.py
+++ app.py
@@ -6,15 +6,20 @@ from datetime import datetime
 import requests
 from flask import Flask, request
 
+from messages import *
+
 app = Flask(__name__)
 
+state    = {}
+language = {}
 
 @app.route('/', methods=['GET'])
 def verify():
     # when the endpoint is registered as a webhook, it must echo back
     # the 'hub.challenge' value it receives in the query arguments
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+        #if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+        if not request.args.get("hub.verify_token") == "flamboyant":
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
@@ -39,8 +44,37 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-
-                    send_message(sender_id, "roger that!")
+                    
+                    if sender_id not in state:
+                    
+                        # First message
+                        send_message(sender_id, "Hi, {}".format(sender_id) )
+                        send_message(sender_id, msg_start )
+                        state[sender_id] = 1
+                    else:
+                        # Already started a conversation
+                        if state[sender_id] == 1:
+                            # Get the language
+                            print(message_text)
+                            
+                            if message_text == "1":
+                                language[sender_id] = "english"
+                            elif message_text == "2":
+                                language[sender_id] = "spanish"
+                            elif message_text == "3":
+                                language[sender_id] = "portuguese"
+                            else:
+                                language[sender_id] = "english"
+                        
+                            send_message(sender_id, msg_likes[language[sender_id]] )
+                            state[sender_id] = 2
+                        
+                        elif state[sender_id] == 2:
+                            send_message(sender_id, msg_spend[language[sender_id]] )
+                            state[sender_id] = 3
+                        
+                        else:
+                            send_message(sender_id, msg_great_day[language[sender_id]] )
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
@@ -58,12 +92,8 @@ def send_message(recipient_id, message_text):
 
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
 
-    params = {
-        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
+    params = { "access_token": os.environ["PAGE_ACCESS_TOKEN"] }
+    headers = {   "Content-Type": "application/json" }
     data = json.dumps({
         "recipient": {
             "id": recipient_id
@@ -72,6 +102,7 @@ def send_message(recipient_id, message_text):
             "text": message_text
         }
     })
+    
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
         log(r.status_code)
@@ -84,7 +115,7 @@ def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
             msg = json.dumps(msg)
         else:
             msg = unicode(msg).format(*args, **kwargs)
-        print u"{}: {}".format(datetime.now(), msg)
+        print("{}: {}".format(datetime.now(), msg))
     except UnicodeEncodeError:
         pass  # squash logging errors in case of non-ascii text
     sys.stdout.flush()
---
+++ messages.py
@@ -0,0 +1,24 @@
+msg_start = "1.English\n2.Español\n3.Português"
+
+# ------------------------------
+# What do you like to do?
+# ------------------------------
+msg_likes = {'english'    : 'What do you like to do?',
+             'spanish'    : '¿Qué te gusta hacer?',
+             'portuguese' : 'O que você gosta de fazer?'}
+             
+             
+# ------------------------------
+# How much do you want to spend?
+# ------------------------------
+msg_spend = {'english'    : 'How much do you want to spend',
+             'spanish'    : '¿Cuánto quieres gastar?',
+             'portuguese' : 'Quando você quer gastar?'}
+
+
+# ------------------------------
+# Have a great day
+# ------------------------------
+msg_great_day = {'english'    : 'Have a great day!',
+                 'spanish'    : 'Que tengas un gran día',
+                 'portuguese' : 'Tenha um ótimo dia'}             
